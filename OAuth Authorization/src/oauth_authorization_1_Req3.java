71ec3f4fcfb6bf3b7ae10bcd73fd205654012318
szmousavi <39388658+szmousavi@users.noreply.github.com>
2023-10-04 08:35:03 +1030

    Update oauth_authorization_1_Req3.java

--- OAuth Authorization/src/oauth_authorization_1_Req3.java
+++ OAuth Authorization/src/oauth_authorization_1_Req3.java
@@ -1,6 +1,6 @@
-// Task: Develop a Java program to obtain permission from users to store files in their Google Drives. 
+// Task: oauth_authorization_1
+// Develop a Java program to obtain permission from users to store files in their Google Drives. 
 // The primary goal is to solely implement the authorization functionality, and other operations, such as file storage, are not to be included in this task.
-
 // Note: The application has been already registered with the Google API, and a Client ID and Client Secret are accessible.
     
 

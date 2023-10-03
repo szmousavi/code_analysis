a9759f67fa1447eac2b17bd7c054ba2eb97baf6e
szmousavi <39388658+szmousavi@users.noreply.github.com>
2023-10-04 08:46:51 +1030

    Update oauth_authorization_3_Req7.java

--- OAuth Authorization/src/oauth_authorization_3_Req7.java
+++ OAuth Authorization/src/oauth_authorization_3_Req7.java
@@ -1,3 +1,9 @@
+// Task: oauth_authorization_2
+// Task 12.3:  Develop a Java program for a "Social Networking" application that facilitates contact suggestions to the user by accessing their Google contacts, with their permission. 
+// The primary goal is to exclusively focus on implementing the authorization functionality, and other operations, such as contact suggestion, are not within the scope of this task.
+// Note: The application has been already registered with the Google API, and a Client ID and Client Secret are accessible.
+    
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;

fa9ef9745644dfcd1ff4b39993e535a113f08625
szmousavi <39388658+szmousavi@users.noreply.github.com>
2023-10-10 09:55:30 +1030

    Update oauth_authentication_1_Req1.java

--- OAuth Authorization/src/oauth_authentication_1_Req1.java
+++ OAuth Authorization/src/oauth_authentication_1_Req1.java
@@ -1,3 +1,8 @@
+// Task: oauth_authentication_1
+// Develop a Java program to integrate Google Single-Sign-On for user authentication into a web application. 
+// The objective is to exclusively implement the authentication functionality, excluding other tasks like designing the user interface. 
+// Note: The application has been already registered with the Google API and the necessary information (e.g., redirect URI, client ID, client secret) has been obtained."
+
 import com.google.api.client.googleapis.auth.oauth2.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
